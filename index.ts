5aa6bbc7d0a468b42187dfbfdd84b3541921e028
b035 <marlon@erler.cc>
2023-02-05 09:43:14 +0100

    added registry

--- index.ts
+++ index.ts
@@ -2,24 +2,88 @@ import Fs from "fs/promises";
 import Path from "path";
 
 // Log
-enum LogType {
+enum LogTypes {
 	status,
 	error,
 	other,
 }
 
-export function log(type: LogType, msg: string) {
+export function log(type: LogTypes, msg: string) {
 	let msg = `TYPE ${type}\n${msg}`;
 }
 
+// Registry
+enum RegistryExitCodes {
+	ok = 0,
+	err_unknown = 1,
+	err_read = 2,
+	err_write = 3,
+	err_del = 4,
+}
+
+interface RegistryResult<T> {
+	code: RegistryExitCodes,
+	value: T,
+}
+
+export const Registry = {
+	base_path: "registry",
+	full_path: (path: string) => Path.join(Registry.base_path, path),
+
+	async write(path: string, content: string): Promise<RegistryResult<null>> {
+		try {
+			await Fs.writeFile(Registry.full_path(path), content);
+			return {
+				code: RegistryExitCodes.ok,
+				value: null,
+			};
+		} catch {
+			return {
+				code: RegistryExitCodes.err_write,
+				value: null,
+			};
+		}
+	},
+
+	async read(path: string): Promise<RegistryResult<string|null>> {
+		try {
+			let text = await Fs.readFile(Registry.full_path(path), { encoding: "utf8" });
+			return {
+				code: RegistryExitCodes.ok,
+				value: text,
+			}
+		} catch {
+			return {
+				code: RegistryExitCodes.err_read,
+				value: null,
+			}
+		}
+	},
+
+	async delete(path: string): Promise<RegistryResult<null>> {
+		try {
+			await Fs.rm(Registry.full_path(path));
+			return {
+				code: RegistryExitCodes.ok,
+				value: null,
+			}
+		} catch {
+			return {
+				code: RegistryExitCodes.err_del,
+				value: null,
+			}
+		}
+	},
+}
+
 // Shell
-enum ShellAction {
+enum ShellActions {
 	exec,
 	bg,
 	kill,
 }
 
-export function shell(action: ShellAction, cmd: string) {
+export function shell(action: ShellActions, cmd: string) {
 	console.log(`${action}:${cmd}`);
 
 	//TODO hanle output
